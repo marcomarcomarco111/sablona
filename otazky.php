8542b54c5dc9d895f7726b61b1bbce43021967fd
marcomarcomarco111 <marcogundait@gmail.com>
2025-03-05 06:28:22 -0800

    qna
    otazky a odpovede

---
+++ otazky.php
@@ -0,0 +1,20 @@
+<?php
+
+$otazky =  array(
+        "Ahoj jak sa mas?",
+        "Ake je pocasie?",
+        "Co mam robit ked mi to nejde?",
+
+
+
+
+);
+
+$odpovede = array (
+        "Pohode",
+    "Slnecno konecne",
+    "Vypni pocitac",
+
+
+);
+
